be34bc465ad06e26519d35735e06793fb29c621a
Nav1 <snithfferx@outlook.com>
2024-11-25 23:14:09 -0600

    Adding configs constants

---
+++ src/configs/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = import.meta.env.APP_NAME;
+export const APP_DESCRIPTION = import.meta.env.APP_DESCRIPTION;
+export const APP_VERSION = import.meta.env.APP_VERSION;
+export const BASE_URL = import.meta.env.BASE_URL;
+export const API_URL = `${import.meta.env.API_URL}/api/`;
