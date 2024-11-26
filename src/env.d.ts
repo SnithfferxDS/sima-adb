1ffdecbe76fade7687c2aa6e40a6b9289a9b3a68
Nav1 <snithfferx@outlook.com>
2024-11-25 23:00:00 -0600

    Updating configs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly APP_NAME: string;
+    readonly APP_DESCRIPTION: string;
+    readonly APP_VERSION: string;
+    readonly APP_URL: string;
+    readonly API_URL: string;
+    readonly API_KEY: string;
+    readonly APP_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
