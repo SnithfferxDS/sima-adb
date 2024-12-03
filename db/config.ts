36f8c6f04c7fdf4c3f88037ef402a1001973a52d
JorgeDS <jrivera@piensads.com>
2024-12-03 17:57:06 -0600

    Updating DB

--- db/config.ts
+++ db/config.ts
@@ -78,11 +78,24 @@ const Person = defineTable({
 });
 
 const SidebarMenu = defineTable({
+  deprecated: true,
   columns: {
     id: column.number({ primaryKey: true }),
     name: column.text(),
     url: column.text(),
     icon: column.text(),
+    submenu: column.json({ optional: true }),
+    created_at: column.date({ default: NOW }),
+    updated_at: column.date({ optional: true })
+  }
+});
+
+const SidebarMenu = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    url: column.text({ optional: true }),
+    icon: column.text({ optional: true }),
     submenu: column.json({ optional: true }),
     created_at: column.date({ default: NOW }),
     updated_at: column.date({ optional: true })
