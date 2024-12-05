9095b7c8da26a371ebf15bf0a718d671538f6e8f
JorgeDS <jrivera@piensads.com>
2024-12-05 17:53:54 -0600

    Updating categories

--- src/pages/api/categories/index.ts
+++ src/pages/api/categories/index.ts
@@ -1,20 +1,38 @@
 import type { APIRoute } from 'astro';
-import { db, Category } from 'astro:db';
+import { db, Category, DaiCategoryProduct } from 'astro:db';
 import { generateSlug } from '../../../lib/utils/slug';
 
 export const POST: APIRoute = async ({ request }) => {
   try {
     const data = await request.json();
+    console.log(data);
     const category = await db.insert(Category).values({
       name: data.name,
       slug: generateSlug(data.name),
       description: data.description.trim() || null,
       parents: JSON.stringify(data.parents),
-      active: data.active ?? true
-    });
+      active: data.active ?? true,
+    }).returning({id: Category.id});
 
-    return new Response(JSON.stringify({ success: true, category }), {
-      status: 201,
+    if (category.length > 0) {
+      const daiCat = await db.insert(DaiCategoryProduct).values({
+        category_id: category[0].id,
+        dai: data.dai,
+        eco_tax: data.eco,
+        licenses: data.lic,
+      });
+      if (daiCat.rowsAffected > 0) {
+        return new Response(JSON.stringify({ success: true, category }), {
+          status: 201,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      }
+    }
+
+    return new Response(JSON.stringify({ error: 'Category creation failed' }), {
+      status: 400,
       headers: {
         'Content-Type': 'application/json',
       },
