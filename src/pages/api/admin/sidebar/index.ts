e61e26484dd22897df4001c998776cd87e00d983
Nav1 <snithfferx@outlook.com>
2024-12-11 01:51:26 -0600

    Adding deleting and editing sidebar menu items

--- src/pages/api/admin/sidebar/index.ts
+++ src/pages/api/admin/sidebar/index.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { db, SidebarMenu } from "astro:db";
+import { db,eq, SidebarMenu } from "astro:db";
 
 /* export const get: APIRoute = async ({ request }) => {
     const { search } = await request.json();
@@ -35,10 +35,56 @@ export const POST: APIRoute = async ({ request }) => {
         return new Response(JSON.stringify({ success: true, sidebarMenu }), { status: 201 });
     } catch (error) {
         console.log(error);
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
         return new Response(
             JSON.stringify(
-                { error: error.message, success: false }
+                { error: errorMessage, success: false }
             ),
             { status: 500 });
     }
+};
+
+export const PUT: APIRoute = async ({ request }) => {
+    try {
+        const { id,name, url, icon, parent_id, position } = await request.json();
+        console.log(id,name, url, icon, parent_id, position);
+        let parent = parent_id > 0 ? parent_id : null;
+        const sidebarMenu = await db
+            .update(SidebarMenu)
+            .set({
+                name,
+                url,
+                icon,
+                parent_id: parent,
+                position,
+            })
+            .where(eq(SidebarMenu.id, Number(id)));
+		console.log(sidebarMenu.rowsAffected);
+        return new Response(JSON.stringify({ success: true, sidebarMenu }), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+        return new Response(
+            JSON.stringify(
+                { error: errorMessage, success: false }
+            ),
+            { status: 500 });
+    }
+};
+
+export const DELETE: APIRoute = async ({request}) => {
+	try {
+		const { id } = await request.json();
+		// console.info("id", id);
+    await db.delete(SidebarMenu).where(eq(SidebarMenu.id, parseInt(id!)));
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+        return new Response(
+            JSON.stringify(
+                { error: errorMessage, success: false }
+            ),
+            { status: 500 });
+  }
 };
