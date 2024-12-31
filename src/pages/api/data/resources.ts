5535b709efde1c852ccf8b3b183bf490683e8031
JorgeDS <jrivera@piensads.com>
2024-12-30 18:18:32 -0600

    Resolved | Filtering CommonNames by Category

--- src/pages/api/data/resources.ts
+++ src/pages/api/data/resources.ts
@@ -7,91 +7,30 @@ export const POST: APIRoute = async ({ request }) => {
         const formData = await request.json();
         const resource = formData.resource;
         const params = formData.params;
-        /* if (resource === 'products') {
+        if (!resource || !params) return new Response(JSON.stringify({ error: 'Invalid resource' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (resource === 'common_names') {
+            if (params.category_id) {
+                const categoryId = Number(params.category_id);
+                const commonNames = await db.select().from(CommonName).where(eq(CommonName.parent_id, categoryId)).all();
+
+                return new Response(
+                    JSON.stringify({ success: true, common_names: commonNames }), {
+                    status: 200,
+                    headers: { 'Content-Type': 'application/json' }
+                });
+            }
+        } else if (resource === 'products') {
             if (params.name) {
                 const productsByName = await db.select().from(Product).where(eq(Product.name, params.name));
                 return new Response(JSON.stringify(productsByName), {
                     status: 200,
                     headers: { 'Content-Type': 'application/json' }
                 });
-            } else {
-                const resources = await db.select().from(Product).all();
-                return new Response(JSON.stringify(resources), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
             }
-        } else if (resource === 'categories') {
-            if (params.name) {
-                const categoriesByName = await db.select().from(Category).where(eq(Category.name, params.name));
-                return new Response(JSON.stringify({ success: true, categories: categoriesByName }), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            } else if (params.id) {
-                const categoriesById = await db.select().from(Category).where(eq(Category.id, params.id));
-                return new Response(JSON.stringify({ success: true, categories: categoriesById }), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            } else {
-                const resources = await db.select().from(Category).all();
-                return new Response(JSON.stringify({ success: true, categories: resources }), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            }
-        } else if (resource === 'common_names') {
-            if (params.name) {
-                const commonNamesByName = await db.select().from(CommonName).where(eq(CommonName.name, params.name));
-                return new Response(JSON.stringify({ success: true, common_names: commonNamesByName }), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            } else if (params.id) {
-                const commonNamesById = await db.select().from(CommonName).where(eq(CommonName.id, params.id));
-                return new Response(
-                    JSON.stringify({ success: true, common_names: commonNamesById }), {
-                    status: 200,
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            } else if (params.category_id) {
-                console.log(commonNamesByCategoryId);
-                */
-        const commonNames = await db.select().from(CommonName).all();
-        if (params.category_id) {
-            let categoryId = Number(params.category_id);
-            console.log("categoryId: ", categoryId);
-            const commonNamesByCategoryId = await Promise.all(
-                // Filter common names by category id
-                commonNames.map(async (commonName: any) => {
-                    console.log("CommonName: ", commonName);
-                    // Get categories array from common name categories json
-                    const categories = JSON.parse(String(commonName.categories));
-                    console.log("Categories: ", categories);
-                    // Check if category id is included in categories array
-                    if (categories.includes(categoryId)) {
-                        console.log("Including common name: ", commonName);
-                        return commonName;
-                    }
-                })
-            );
-            return new Response(
-                JSON.stringify({ success: true, common_names: commonNamesByCategoryId }), {
-                status: 200,
-                headers: { 'Content-Type': 'application/json' }
-            });
         }
-        /*
-           } else {
-               const resources = await db.select().from(Category).all();
-               return new Response(
-                   JSON.stringify({ success: true, common_names: resources }), {
-                   status: 200,
-                   headers: { 'Content-Type': 'application/json' }
-               });
-           }
-       } */
         return new Response(JSON.stringify({ error: 'Invalid resource' }), {
             status: 400,
             headers: { 'Content-Type': 'application/json' }
