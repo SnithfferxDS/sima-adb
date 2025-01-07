1de1b183996d8226d450f3efa7fdefa2ca74d85e
JorgeDS <jrivera@piensads.com>
2025-01-06 18:07:49 -0600

    Changing db connection credentials

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,13 @@
 import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 import * as schema from '@DB/schema';
+import { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } from '@Configs/constants';
 
 const connection = await mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: DATABASE_HOST,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
 });
 
 export const db = drizzle(connection, { schema, mode: 'default' });
