f9fda5deccebafce76f6dbf0956152bcdcc6645c
JorgeDS <jrivera@piensads.com>
2025-01-07 13:44:42 -0600

    User Migration ready

--- src/pages/api/auth/user/migrate.ts
+++ src/pages/api/auth/user/migrate.ts
@@ -2,32 +2,17 @@ import type { APIRoute } from "astro";
 import { API_URL } from "@Configs/constants";
 import { db, Person, User } from "astro:db";
 import { generateUserId, hashPassword } from "@Lib/auth";
+import { sendRegisterNotification } from "@Actions/email/AuthRegisterNotification";
 
 export const POST: APIRoute = async ({ request }) => {
     try {
         const data = await request.json();
         // get user from API
-        const dsUser = await fetch(`${API_URL}/api/user/migrate?term=${data.userName}`)
+        const dsUser = await fetch(`${API_URL}/users/migrate?terms=${data.userName}`)
             .then((res) => res.json());
         const hashedPassword = await hashPassword(data.password);
         if (dsUser.length > 0) {
-            let userName = "", userLastName = "";
             const userPersonName = dsUser[0].name.split(" ");
-            if (userPersonName.length == 1) {
-                userName = userPersonName[0];
-            } else if (userPersonName.length == 2) {
-                userName = userPersonName[0];
-                userLastName = userPersonName[1];
-            } else if (userPersonName.length == 3) {
-                userName = userPersonName[0];
-                userLastName = userPersonName[1] + " " + userPersonName[2];
-            } else if (userPersonName.length == 4) {
-                userName = userPersonName[0] + " " + userPersonName[1];
-                userLastName = userPersonName[2] + " " + userPersonName[3];
-            } else {
-                userName = userPersonName[0] + " " + userPersonName[1];
-                userLastName = userPersonName[2] + " " + userPersonName[3];
-            }
             const person = await db.insert(Person).values({
                 first_name: userPersonName[0],
                 second_name: userPersonName[1] ?? '',
@@ -38,19 +23,20 @@ export const POST: APIRoute = async ({ request }) => {
                 email: dsUser[0].email,
                 phone: dsUser[0].phone || '',
             }).returning();
-            if (person !== undefined) {
+            if (person) {
+                console.log(person[0].id);
                 const user = await db.insert(User).values({
                     id: generateUserId(),
                     name: dsUser[0].usuario,
                     email: dsUser[0].email,
                     password: hashedPassword,
-                    person_id: person[0].id,
                     phone: dsUser[0].phone || '',
+                    person: person[0].id,
                     level: dsUser[0].idNivel,
                     email_verified: false,
                 }).returning();
-                if (user !== undefined) {
-                    const dsapiUser = await fetch(`${API_URL}/api/auth/register/`, {
+                if (user) {
+                    const dsapiUser = await fetch(`${API_URL}/auth/register/`, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
@@ -58,16 +44,27 @@ export const POST: APIRoute = async ({ request }) => {
                         body: JSON.stringify({
                             name: dsUser[0].usuario,
                             email: dsUser[0].email,
-                            password: hashedPassword,
+                            password: data.password,
                         }),
                     });
-                    return new Response(JSON.stringify({ success: true }), {
-                        status: 201,
-                        headers: {}
-                    });
+                    sendRegisterNotification({
+                        email: dsUser[0].email,
+                        username: dsUser[0].usuario,
+                        activationLink: dsapiUser.url
+                    })
+                } else {
+                    throw new Error('Failed to create user');
                 }
+            } else {
+                throw new Error('Failed to create Person');
             }
+        } else {
+            throw new Error('Failed to retrieve user from DSIN');
         }
+        return new Response(JSON.stringify({ success: true }), {
+            status: 201,
+            headers: {}
+        });
     } catch (error) {
         console.log(error);
         const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
