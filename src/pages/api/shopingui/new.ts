3964f691299709c04bbb748337d8b734211759e0
JorgeDS <jrivera@piensads.com>
2025-01-13 14:02:02 -0600

    Agregando shopingui como módulo

--- src/pages/api/shopingui/new.ts
+++ src/pages/api/shopingui/new.ts
@@ -1,9 +1,19 @@
+import { API_URL } from '@Configs/constants';
 import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async ({ request }) => {
     try {
         const data = await request.json();
-        console.log(data);
+        const product = await fetch(`${API_URL}/products/migrate`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                product: data.product,
+            }),
+        });
+        if (!product.ok) throw new Error("Failed to create product");
         return new Response(JSON.stringify({ success: true }), { status: 201 });
     } catch (error) {
         return new Response(JSON.stringify({ error: 'Failed to create product' }), { status: 500 });
