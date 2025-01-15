78642b58587a10328191df3be7738a9831102320
JorgeDS <jrivera@piensads.com>
2025-01-15 14:08:14 -0600

    Changing authentication endpoint resolver

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,35 +1,52 @@
-import type { APIRoute} from "astro";
-import {db, eq, or, Person, User} from "astro:db";
-import {authenticateUser, comparePasswords, generateUserId, hashPassword} from "../../../lib/auth";
-import { createSession, setSessionCookie, generateCsrfToken, setCsrfCookie } from '../../../lib/session';
+import type { APIRoute } from "astro";
+import { authenticateUser, comparePasswords, generateUserId, generateSignature, saveApiToken } from "../../../lib/auth";
+import { createSession, setSessionCookie } from '../../../lib/session';
+import { API_URL } from "@Configs/constants";
 
-export const POST: APIRoute = async ({ request })=>{
+export const POST: APIRoute = async ({ request }) => {
     try {
         const formData = await request.json();
         const email = formData.terms;
-        const userName= formData.terms;
+        const userName = formData.terms;
         const password = formData.password;
 
         if (!email || !userName || !password) {
-            return new Response(JSON.stringify({ error: 'Email and password are required'}), { status: 400 });
+            return new Response(JSON.stringify({ error: 'Email and password are required' }), { status: 400 });
         } else {
-            const user = await authenticateUser(email,userName,password);
+            const user = await authenticateUser(email, userName, password);
             if (!user) {
-                return new Response(JSON.stringify({ error:'Invalid email or password'}), { status: 400 });
+                return new Response(JSON.stringify({ error: 'Invalid credentials' }), { status: 400 });
             } else {
                 const isValidPassword = await comparePasswords(password, user.password);
                 if (!isValidPassword) {
-                    return new Response(JSON.stringify({ error: 'Invalid email or password'}), { status: 400 });
+                    return new Response(JSON.stringify({ error: 'Invalid credentials' }), { status: 400 });
                 } else {
+                    // Generate signature
+                    const signature = await generateSignature(user.name);
                     // Create session
                     const sessionToken = await createSession({
-                        userId: user.id,
-                        email: user.email,
-                        name: user.name
+                        id: user.id,
+                        level: user.level,
+                        signature: signature
                     });
                     const cookie = setSessionCookie(sessionToken);
+                    // Login user on API
+                    const apiToken = await fetch(`${API_URL}/auth/login`, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            name: userName,
+                            email: email,
+                            password: password
+                        }),
+                    }).then(res => res.json());
+                    if (!apiToken) throw new Error("Failed to login user on API");
+                    const token = apiToken.token;
+                    saveApiToken(user.id, token);
                     return new Response(
-                        JSON.stringify({success: true, cookie}),
+                        JSON.stringify({ success: true, cookie }),
                         {
                             status: 201,
                             headers: {
@@ -42,9 +59,9 @@ export const POST: APIRoute = async ({ request })=>{
         }
         //return new Response(JSON.stringify({error: "Something went wrong, please try again later"}),{status:500});
     } catch (e) {
-        console.error("Error: ",e);
+        console.error("Error: ", e);
         return new Response(
-            JSON.stringify({error:'An error occurred during login'}),
+            JSON.stringify({ error: 'An error occurred during login' }),
             { status: 400, statusText: "Invalid email or password", headers: { "content-type": "application/json" } });
     }
 }
