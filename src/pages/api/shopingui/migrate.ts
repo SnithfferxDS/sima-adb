614973ca23e6f683ba509a649aac600e890ba92c
JorgeDS <jrivera@piensads.com>
2025-01-23 14:21:54 -0600

    updating product migrations

--- src/pages/api/shopingui/migrate.ts
+++ src/pages/api/shopingui/migrate.ts
@@ -1,5 +1,9 @@
-import { API_URL } from '@Configs/constants';
+import { API_URL, API_EMAIL, API_PASS, API_USER } from '@Configs/constants';
+import type { ShopinguiProduct } from '@Types/shopingui/products/ProductSchema';
 import type { APIRoute } from 'astro';
+import { ShopinguiService } from 'src/services/shopingui.service';
+
+const shopinguiServices = new ShopinguiService();
 
 export const POST: APIRoute = async ({ request }) => {
     try {
@@ -10,7 +14,7 @@ export const POST: APIRoute = async ({ request }) => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                product: data.product,
+                page: data.product,
             }),
         });
         if (!product.ok) throw new Error("Failed to create product");
@@ -18,4 +22,69 @@ export const POST: APIRoute = async ({ request }) => {
     } catch (error) {
         return new Response(JSON.stringify({ error: 'Failed to create product' }), { status: 500 });
     }
+}
+
+export const GET: APIRoute = async ({ request }) => {
+    try {
+        let token = '';
+        const authResponse = await fetch(`${API_URL}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify({
+                name: API_USER,
+                email: API_EMAIL,
+                password: API_PASS,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .catch((err) => console.log("Error: ", err));
+        if (authResponse && authResponse.token) {
+            token = authResponse.token;
+        }
+        // Loop through all products
+        let currentPage = 1, totalPages = 0;
+        do {
+            const response = await getProducts(token, currentPage);
+            // console.log(response);
+            if (response.ok) {
+                console.info("Initiating migration of page: ", currentPage);
+                const productsData = await response.json();
+                if (currentPage == 1) totalPages = parseInt(productsData.max);
+                currentPage += parseInt(productsData.current);
+                try {
+                    const products = await Promise.all(
+                        productsData.data.map(async (product: ShopinguiProduct) =>
+                            await shopinguiServices.addProductFromDS(product))
+                    )
+                        .catch((err) => console.log("Error: ", err));
+                    if (products) {
+                        console.info("Product Migrated: ", currentPage);
+                    }
+                } catch (error) {
+                    throw new Error("Error: " + error);
+                }
+            } else {
+                if (currentPage < totalPages) {
+                    throw new Error("Failed to create product");
+                } else {
+                    console.info("All products migrated");
+                    break;
+                }
+            }
+        } while (currentPage <= totalPages);
+        return new Response(JSON.stringify({ success: true }), { status: 201 });
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Failed to create product' }), { status: 500 });
+    }
+}
+
+async function getProducts(token: string, currentPage: number) {
+    console.log("Current page: ", currentPage);
+    return await fetch(`${API_URL}/products/migrate?page=${currentPage}`, {
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+    });
 }
